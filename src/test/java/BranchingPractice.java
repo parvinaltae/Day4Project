55549c54ccede96ea16907fd114b149fce7ab08e
Parvin <parvin.altae@gmail.com>
2021-05-25 19:23:42 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,9 @@
 public class BranchingPractice {
 
     public static void main(String[] args) {
+    // created new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
 
     }
 }
